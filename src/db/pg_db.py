0037e30ece849843123131c6f9878d380011d5c9
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-07-07 10:29:25 +0000

    feat: add pg async db module

---
+++ src/db/pg_db.py
@@ -0,0 +1,44 @@
+"""Postgresql db module."""
+
+import rootutils
+
+ROOT = rootutils.autosetup()
+
+from sqlmodel import Session, SQLModel, create_engine, text
+
+from src.utils.logger import get_logger
+
+log = get_logger()
+
+
+class PgSyncDb:
+    """Postgresql syncronous db module."""
+
+    def __init__(self, host: str, port: int, user: str, password: str, db: str) -> None:
+        """Initialize SQL database."""
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db = db
+
+        self.url = f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+    def setup(self) -> None:
+        """Setup SQL database."""
+        log.log(22, f"Connecting SQL to {self.host}:{self.port}/{self.db}...")
+        self.engine = create_engine(
+            url=self.url,
+            echo=False,
+        )
+        self.session = Session(self.engine)
+
+        # test connection
+        self.session.exec(text("SELECT 1"))
+
+        log.log(22, f"Connected SQL to {self.host}:{self.port}/{self.db}")
+
+    def create_all(self) -> None:
+        """Create all tables."""
+        log.log(22, "Creating all tables...")
+        SQLModel.metadata.create_all(self.engine)
